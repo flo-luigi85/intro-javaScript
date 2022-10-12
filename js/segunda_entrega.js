b4a8eb8c70ebd3d4766434f51869924111b39e06
flo-luigi85 <flo.lugi85@gmail.com>
2022-10-11 22:28:43 -0300

    terminando

--- js/segunda_entrega.js
+++ js/segunda_entrega.js
@@ -18,14 +18,15 @@ class Prenda {
 }
 
 const prendas = [
-    new Prenda('Campera', 'keira', 'Abrigo', 1, 'Invierno', 5, 1),
+    new Prenda('Campera', 'keira', 'Abrigo', 1, 'Invierno', 8, 1),
     new Prenda('Campera', 'Trivor', 'Abrigo', 2, 'Invierno', 9, 2),
-    new Prenda('Pantalon', 'Urbano', 'Liviano', 4, 'Verano', 6, 3),
-    new Prenda('Pantalon', 'Trekking', 'Outdoor', 3, 'Invierno', 5, 4),
-    new Prenda('Camisa', 'Sydney', 'Liviano', 3, 'Verano', 4, 5),
-    new Prenda('Remera', 'Icon', 'Liviano', 6, 'Verano', 3, 6),
-    new Prenda('Remera', 'Asterix', 'Outdoor', 7, 'Invierno/verano', 2, 7),
-    new Prenda('Campera/polar', 'Ben', 'Outdoor', 7, 'Invierno/verano', 3, 8)
+    new Prenda('Campera', 'Ben', 'Outdoor', 7, 'Invierno/verano', 3, 3),
+    new Prenda('Campera', 'Urbano', 'Liviano', 4, 'Verano', 5, 4),
+    new Prenda('Campera', 'Trekking', 'Outdoor', 3, 'Invierno', 6, 5),
+    new Prenda('Campera', 'Sydney', 'Liviano', 3, 'Verano', 3, 6),
+    new Prenda('Campera', 'Icon', 'Liviano', 6, 'Verano', 4, 7),
+    new Prenda('Campera', 'Asterix', 'Outdoor', 7, 'Invierno/verano', 3, 8)
+    
 
 ]
 console.log(prendas);
@@ -35,7 +36,7 @@ console.log(prendas);
 let continuar = true;
 
 while (continuar) {
-    let ingreso = prompt('Ingresa el nombre de las camperas/pantalon/camisa/remera: Tipo de prenda, Modelo, Tipo, Talle(de 1 a 7), Temporada, Valoracion, Separados por una barra diagonal (/) Teclea S para salir');
+    let ingreso = prompt('Ingresa el nombre de las camperas: Tipo de prenda, Modelo, Tipo(Abrigo/Liviano/Urbano/Outdoor), Talle(de 1 a 7), Temporada(verano/invierno), Valoracion, Separados por una barra diagonal (/) Teclea S para salir');
                         
     if (ingreso.toUpperCase() == 'S') {
         continuar = false;
@@ -49,4 +50,54 @@ while (continuar) {
     prenda.asignarId(prendas);
     console.log(prendas);
 } 
+//=====================
+let criterio = prompt('Elija una opción:\n1 - Campera \n2 - Mejor a peor puntuado \n3 - Talle(Más viejo a más nuevo)');
 
+function ordenar(criterio, array) {
+    let arrayOrdenado = array.slice(0);
+
+
+    switch (criterio) {
+        case '1':
+            let nombreAscendente = arrayOrdenado.sort((a,b)=>a.nombre.localeCompare(b.nombre));
+            return nombreAscendente;        
+        case '2':
+            return arrayOrdenado.sort((a, b) => b.valoracion - a.valoracion);
+        case '3':
+            return arrayOrdenado.sort((a, b) => b.talle - a.talle);
+        default:
+            alert('No es un criterio válido');
+            break;
+    }
+}
+
+//============================================
+
+function crearStringResultado(array){
+    let info = '';
+
+    array.forEach(elemento=>{
+        info += 'Nombre: ' + elemento.nombre + '\nCampera: ' + elemento.modelo + '\nValoración: ' + elemento.valoracion + ' puntos.\n\n'
+    })
+
+    return info;
+}
+
+//====================================
+
+alert(crearStringResultado(ordenar(criterio,prendas)));
+
+//===============================================
+
+let modeloElegido = prompt('Escribí el modelo de prenda para que te mostremos');
+
+const filtrado = prendas.filter((prenda)=>prenda.modelo.toLowerCase().includes(modeloElegido.toLowerCase()))
+
+//====================================
+
+if (filtrado.length==0){
+    alert('Lo sentimos. Por ahora no esta en nuestro catálogo');
+}else{
+    const imprimible = filtrado.map((prenda)=>prenda.nombre);
+    alert('Las prendas de nuestro catálogo, concuerdan con:\n- ' + imprimible.join('\n- '));
+}
